1546613f2f06bbc23d47994f3ee61cf8676bce01
Rahul Bhoje <rahulbhoje09@gmail.com>
2025-06-11 10:59:54 +0530

    Register Order service to eureka service registry

--- src/main/java/com/javatechie/config/ApplicationConfig.java
+++ src/main/java/com/javatechie/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package com.javatechie.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
