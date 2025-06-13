e3098975b9986fb4aa667b749fa2de7db39e67b4
Rahul Bhoje <rahulbhoje09@gmail.com>
2025-06-13 18:26:44 +0530

    Centralize configuration using Spring cloud config server

--- src/main/java/com/javatechie/service/OrderService.java
+++ src/main/java/com/javatechie/service/OrderService.java
@@ -11,6 +11,7 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -21,16 +22,26 @@ import java.util.UUID;
 
 @Service
 @Slf4j
+@RefreshScope
 public class OrderService {
 
-    public static final String PAYMENT_SERVICE_URL = "http://PAYMENT-SERVICE/payments";
-    public static final String USERS_SERVICE_URL = "http://USER-SERVICE/users";
+//    public static final String PAYMENT_SERVICE_URL = "http://PAYMENT-SERVICE/payments";
+//    public static final String USERS_SERVICE_URL = "http://USER-SERVICE/users";
     @Autowired
     private OrderRepo orderRepo;
 
     @Autowired
     @Lazy
     private RestTemplate restTemplate;
+
+    @Value("${microservice.payment-service.endpoints.fetchPaymentById.uri}")
+    private String paymentServiceUri;
+
+    @Value("${microservice.user-service.endpoints.fetchUserById.uri}")
+    private String userServiceUri;
+
+    @Value("${test.value}")
+    private String test;
 
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
@@ -54,11 +65,12 @@ public class OrderService {
 
     @CircuitBreaker(name = "orderService", fallbackMethod = "getOrderDetails")
     public OrderResponseDTO getOrder(String orderId){
+        System.out.println("************* "+test);
         Order order = orderRepo.findByOrderId(orderId);
 
-        PaymentDTO payment = restTemplate.getForObject(PAYMENT_SERVICE_URL + "/" +orderId,PaymentDTO.class);
+        PaymentDTO payment = restTemplate.getForObject(paymentServiceUri + "/" +orderId,PaymentDTO.class);
 
-        UserDTO user = restTemplate.getForObject(USERS_SERVICE_URL + "/" +order.getUserId(), UserDTO.class);
+        UserDTO user = restTemplate.getForObject(userServiceUri + "/" +order.getUserId(), UserDTO.class);
 
         return OrderResponseDTO.builder()
                 .order(order)
